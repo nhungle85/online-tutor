6c11d7a3baa19fce28d6921ec480198a31e550dc
nhungle <misssg85@gmail.com>
2021-10-16 21:11:52 -0700

    show/hide brand in navbar + create card links on courses

--- js/script.js
+++ js/script.js
@@ -2,5 +2,19 @@ $(function() {
    $("#loginButton").click(function() {
         $("#loginModal").modal("show");
    });
-   
+
+   const distance = $('#navBar').offset().top,
+    $window = $(window);
+
+    $window.scroll(function() {
+    if ( $window.scrollTop() >= distance ) {
+        // navbar has reached the top
+        $('#navBarBrand').removeClass('d-none');
+    } else {
+        if (!$('#navBarBrand').hasClass('d-none')) {
+            $('#navBarBrand').addClass('d-none');
+        }
+    }
+    
+});
 });
